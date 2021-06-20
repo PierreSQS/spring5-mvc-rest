ca91791d54c01aa90c24aa0b1d097d784284dadd
PierreSQS <nomo_messina@web.de>
2021-06-20 17:12:40 +0200

    Chap383-Assignment: Optimized CustomerMapper Tests.

--- src/test/java/guru/springfamework/api/v1/mapper/CustomerMapperTest.java
+++ src/test/java/guru/springfamework/api/v1/mapper/CustomerMapperTest.java
@@ -4,24 +4,18 @@ import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.domain.Customer;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class CustomerMapperTest {
 
     private Customer customer;
 
-    @Autowired
     private CustomerMapper custMapper ;
 
     @Before
     public void setUp() {
+        custMapper = CustomerMapper.INSTANCE;
     }
 
     @Test
