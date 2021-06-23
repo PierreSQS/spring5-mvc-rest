d40d10fb0bc035492a4ea396c5b0d28021d3e7d7
PierreSQS <nomo_messina@web.de>
2021-06-23 20:02:16 +0200

    Chap385-Create Customer: Removed unused imports

--- src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
+++ src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,7 +16,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
