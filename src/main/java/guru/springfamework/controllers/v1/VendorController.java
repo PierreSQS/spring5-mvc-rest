ca71c824f22fd9e56c3c30b0507de7cac5f2f4d9
PierreSQS <nomo_messina@web.de>
2021-07-15 18:46:04 +0200

    Chap393-Assn Create Vendors API: Extended Controller and Test for GET ALL Vendors API

--- src/main/java/guru/springfamework/controllers/v1/VendorController.java
+++ src/main/java/guru/springfamework/controllers/v1/VendorController.java
@@ -1,6 +1,7 @@
 package guru.springfamework.controllers.v1;
 
 import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.api.v1.model.VendorListDTO;
 import guru.springfamework.services.VendorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,5 +23,10 @@ public class VendorController {
     @GetMapping("{id}")
     public VendorDTO getVendorByID(@PathVariable Long id) {
         return vendorService.getVendorByID(id);
+    }
+
+    @GetMapping
+    public VendorListDTO getAllVendors() {
+        return vendorService.getAllVendors();
     }
 }
--- src/test/java/guru/springfamework/controllers/v1/VendorControllerTest.java
+++ src/test/java/guru/springfamework/controllers/v1/VendorControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springfamework.controllers.v1;
 
 import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.api.v1.model.VendorListDTO;
 import guru.springfamework.services.VendorService;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,8 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+
 import static guru.springfamework.controllers.v1.VendorController.BASE_URL;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -50,6 +54,26 @@ public class VendorControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name",equalTo(vendorName)))
                 .andExpect(jsonPath("$.vendor_url",equalTo(BASE_URL+"/1")))
+                .andDo(print());
+    }
+
+    @Test
+    public void getAllVendors() throws Exception {
+        // Given
+        VendorDTO vendorDTO1 = new VendorDTO();
+        vendorDTO1.setName("Mock1 Inc.");
+
+        VendorDTO vendorDTO2 = new VendorDTO();
+        vendorDTO2.setName("Mock2 Inc.");
+
+        VendorListDTO vendorListDTO = new VendorListDTO(Arrays.asList(vendorDTO1,vendorDTO2));
+
+        when(vendorService.getAllVendors()).thenReturn(vendorListDTO);
+        // When, Then
+        mockMvc.perform(get(BASE_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.vendors", hasSize(2)))
+                .andExpect(jsonPath("$.vendors[1].name", equalTo(vendorDTO2.getName())))
                 .andDo(print());
     }
 }
