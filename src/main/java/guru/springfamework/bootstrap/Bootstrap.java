b650c50718cbc3fe832be44da0ec105eab379db7
PierreSQS <nomo_messina@web.de>
2021-07-15 14:21:56 +0200

    Chap393-Assn Create Vendors API: Added Vendors load in Bootstrap

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -2,23 +2,29 @@ package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
 import guru.springfamework.domain.Customer;
+import guru.springfamework.domain.Vendor;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
- * Created by jt on 9/24/17.
+ * Refactored by Pierrot on 7/15/21.
  */
 @Component
 public class Bootstrap implements CommandLineRunner{
 
     private final CategoryRepository categoryRepository;
     private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
@@ -26,6 +32,7 @@ public class Bootstrap implements CommandLineRunner{
 
         loadCategories();
         loadCustomers();
+        loadVendors();
     }
 
     private void loadCategories() {
@@ -54,20 +61,32 @@ public class Bootstrap implements CommandLineRunner{
     }
 
     private void loadCustomers() {
-        //given
         Customer customer1 = new Customer();
-        customer1.setId(1L);
         customer1.setFirstname("Michale");
         customer1.setLastname("Weston");
         customerRepository.save(customer1);
 
         Customer customer2 = new Customer();
-        customer2.setId(2L);
         customer2.setFirstname("Sam");
         customer2.setLastname("Axe");
 
         customerRepository.save(customer2);
 
         System.out.println("Customers Loaded: " + customerRepository.count());
+    }
+
+    private void loadVendors() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Mangoes Inc Ltd");
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Guava from Cameroon Ltd.");
+
+        Vendor vendor3 = new Vendor();
+        vendor3.setName("Ghana Papayas Inc.");
+
+        vendorRepository.saveAll(Arrays.asList(vendor1,vendor2,vendor3));
+
+        System.out.println("Vendors Loaded: " + vendorRepository.count());
     }
 }
